657d6dbc7ebf383d4072f2370d9f9b572b86dc8a
Brian Cancel <brian.e.cancel@gmail.com>
2020-08-04 15:52:02 -0400

    removed all console.logs

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 var score = 0;
 var person = prompt("Hey there! What is your preferred name?");
-//prints input value of person after person is prompted
-console.log(person);
 
 if (person !== null) {
   alert("Welcome " + person + "!");
@@ -14,28 +12,22 @@ if (person !== null) {
 
 function greet() {
   var firstQ = prompt("My name is Brian?");
-  //prints input value of firstQ after firstQ is prompted
-  console.log(firstQ);
 
   firstQ = firstQ.toLowerCase();
   if (firstQ === "no") {
-    // console.log(" Wrong! Why do you hate me " + person + "...");
     alert(" Wrong! Why do you hate me " + person + "...");
     quest2();
   }
   if (firstQ === "n") {
-    // console.log("  Wrong! Why do you hate me " + person + "...");
     alert("  Wrong! Why do you hate me " + person + "...");
     quest2();
   }
   if (firstQ === "yes") {
-    // console.log("Correct! I knew you had my back " + person + ".");
     alert("Correct! I knew you had my back " + person + ".");
     score = score + 1;
     quest2();
   }
   if (firstQ === "y") {
-    // console.log("Correct! I knew you had my back " + person + ".");
     alert("Correct! I knew you had my back " + person + ".");
     score = score + 1;
     quest2();
@@ -44,29 +36,23 @@ function greet() {
 
 function quest2() {
   var secondQ = prompt("Was I born in Puerto Rico?");
-  //prints input value of secondQ after secondQ is prompted
-  console.log(secondQ);
 
   secondQ = secondQ.toLowerCase();
   if (secondQ === "yes") {
-    // console.log("Si, muy bien " + person + "!");
     alert("Si, muy bien " + person + "!");
     score = score + 1;
     quest3();
   }
   if (secondQ === "y") {
-    // console.log("Si, muy bien " + person + "!");
     alert("Si, muy bien " + person + "!");
     score = score + 1;
     quest3();
   }
   if (secondQ === "no") {
-    // console.log("Incorrecto! Quizas la proxima.");
     alert("Incorrecto! Quizas la proxima.");
     quest3();
   }
   if (secondQ === "n") {
-    // console.log("Incorrecto! Quizas la proxima.");
     alert("Incorrecto! Quizas la proxima.");
     quest3();
   }
@@ -74,28 +60,22 @@ function quest2() {
 
 function quest3() {
   var thirdQ = prompt("Did I serve in the Army for 4 years?");
-  //prints input value of thirdQ after thirdQ is prompted
-  console.log(thirdQ);
 
   thirdQ = thirdQ.toLowerCase();
   if (thirdQ === "yes") {
-    // console.log("Wrong! 6 years actually " + person + "...");
     alert("Wrong! 6 years actually " + person + "...");
     quest4();
   }
   if (thirdQ === "y") {
-    // console.log(" Wrong! 6 years actually " + person + " ...");
     alert(" Wrong! 6 years actually " + person + " ...");
     quest4();
   }
   if (thirdQ === "no") {
-    // console.log("You are right, 6 years actually! Woot woot");
     alert("You are right, 6 years actually! Woot woot");
     score = score + 1;
     quest4();
   }
   if (thirdQ === "n") {
-    // console.log("You are right, 6 years actually! Woot woot");
     alert("You are right, 6 years actually! Woot woot");
     score = score + 1;
     quest4();
@@ -104,28 +84,23 @@ function quest3() {
 
 function quest4() {
   var fourthQ = prompt("I have 4 siblings?");
-  //prints input value of fourthQ after fourthQ is prompted
   console.log(fourthQ);
 
   fourthQ = fourthQ.toLowerCase();
   if (fourthQ === "no") {
-    // console.log("Wrong! 3 brothers & 1 sister...");
     alert("Wrong! 3 brothers & 1 sister...");
     quest5();
   }
   if (fourthQ === "n") {
-    // console.log("Wrong! 3 brothers & 1 sister...");
     alert("Wrong! 3 brothers & 1 sister...");
     quest5();
   }
   if (fourthQ === "yes") {
-    // console.log(person + ", you love me... I can feel it.");
     alert(person + ", you love me... I can feel it.");
     score = score + 1;
     quest5();
   }
   if (fourthQ === "y") {
-    // console.log(person + ", you love me... I can feel it.");
     alert(person + ", you love me... I can feel it.");
     score = score + 1;
     quest5();
@@ -134,28 +109,22 @@ function quest4() {
 
 function quest5() {
   var fifthQ = prompt("I have a strong dislike for bananas?");
-  //prints input value of fifthQ after fifthQ is prompted
-  console.log(fifthQ);
 
   fifthQ = fifthQ.toLowerCase();
   if (fifthQ === "no") {
-    // console.log("Ewww, I would never come close to a banana");
     alert("Ewww, I would never come close to a banana");
     quest6();
   }
   if (fifthQ === "n") {
-    // console.log("Ewww, I would never come close to a banana");
     alert("Ewww, I would never come close to a banana");
     quest6();
   }
   if (fifthQ === "yes") {
-    // console.log(person + ", you know me too well.");
     alert(person + ", you know me too well.");
     score = score + 1;
     quest6();
   }
   if (fifthQ === "y") {
-    // console.log(person + ", you know me too well.");
     alert(person + ", you know me too well.");
     score = score + 1;
     quest6();
@@ -169,7 +138,6 @@ function quest6() {
         person +
         ", using numbers try an guess how many different questions you have been asked? (hint: single digit number)"
     );
-    console.log(sixthQ);
     var num1 = parseInt(sixthQ);
     if (num1 === 0) {
       alert("Are you serious...  try again and guess higher!");
@@ -236,12 +204,10 @@ function quest7() {
     seventhQ = seventhQ.toLowerCase();
 
     if (places.indexOf(seventhQ) !== -1) {
-      console.log(seventhQ);
       alert("Thats right, I would not even think twice!");
       score = score + 1;
       alert(score);
     } else {
-      console.log(seventhQ);
       alert("I would consdier it, but not a preferred place!");
     }
   }
